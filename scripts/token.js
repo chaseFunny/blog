b51715534181a5ac8eaff6c4b95c7eb39e90fa6f
zhanghao <1601565568@qq.com>
2023-03-30 15:50:22 +0800

    1

---
+++ scripts/token.js
@@ -0,0 +1,6 @@
+const REMOTE_TOKEN = {
+  GH_USER: 'chaseFunny',
+  GH_PROJECT_NAME: 'blog',
+  GH_TOKEN: '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',
+}
+module.exports = { REMOTE_TOKEN }
