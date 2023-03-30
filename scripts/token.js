6e5152b501e9667baef59ed31a0cb4e84bb307ca
chaseSun <75657636+chaseFunny@users.noreply.github.com>
2023-03-30 16:25:04 +0800

    dele

--- scripts/token.js
+++
@@ -1,6 +0,0 @@
-const REMOTE_TOKEN = {
-  GH_USER: 'chaseFunny',
-  GH_PROJECT_NAME: 'blog',
-  GH_TOKEN: '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',
-}
-module.exports = { REMOTE_TOKEN }
